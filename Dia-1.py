d4340ba305809dedeefbedb6b02713e10d362586
Maximo Barraza <megadawnmega@gmail.com>
2026-02-13 13:55:06 -0300

    dia 2

--- Dia-1.py
+++ Dia-1.py
@@ -1,1 +1,15 @@
-print("Hola mundo")
+# Tipos Variables
+
+print(type(1 + 1))
+print(type(1 + 0.1))
+print(type(True))
+print(type(1 + 1 *2j))
+print(type("Hola mundo!"))
+
+# Operadores
+
+print(1 + 1)
+print(1 - 1)
+print(2 * 2)
+print(2 / 2)
+print(10 % 3)
