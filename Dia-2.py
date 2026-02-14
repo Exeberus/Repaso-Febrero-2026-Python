a189771cb28c7f0b9cf77c134abedec3bdc6b2e6
Maximo Barraza <megadawnmega@gmail.com>
2026-02-14 20:26:39 -0300

    Dia 2

--- Dia-2.py
+++ Dia-2.py
@@ -1,14 +1,9 @@
 # Comprensión de Listas.
 
-# Ejemplo
-'''
-numeros = [1, 4, 5, 8, 9]
-numeros_dobles = [i * 2 for i in numeros] #Comprehension
-
-print(numeros_dobles)
-'''
+# Ejemplo.
 
 # No compresión:
+'''
 numeros = [1, 4, 5, 8, 9]
 numeros_dobles = []
 
@@ -17,3 +12,25 @@ for i in numeros:
     numeros_dobles.append(n)
 
 print(numeros_dobles)
+'''
+
+'''# Comprensión
+numeros = [1, 2, 3, 4, 5, 8, 9]
+numeros_dobles = [i * 2 for i in numeros] #Comprehension
+
+print(numeros_dobles)
+
+menor_4 = [i if (i < 4) else '*' for i in numeros]
+print(menor_4)'''
+
+## Practica
+
+numeros_practica_tp1 = [3, 7, 10, 15, 20]
+numeros_mayores_10_tp1 = [n * 3  for n in numeros_practica_tp1 if (n > 10)]
+
+print(numeros_mayores_10_tp1)
+
+numeros_practica_tp2 = [1, 2, 3, 4, 5]
+numeros_impar_o_par_tp2 = ['Par' if (i % 2 == 0) else 'Impar' for i in numeros_practica_tp2]
+
+print(numeros_impar_o_par_tp2)
